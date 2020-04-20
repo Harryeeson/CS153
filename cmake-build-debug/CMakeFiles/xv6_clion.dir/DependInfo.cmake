7b58f1fc3d0f2aa572724cba417c990c5d4cd0f7
Harryeeson <yeeharrison57@yahoo.com>
2020-04-20 15:15:52 -0700

    Upload project with part a

---
+++ cmake-build-debug/CMakeFiles/xv6_clion.dir/DependInfo.cmake
@@ -0,0 +1,67 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "C:/Users/yeeha/CLionProjects/xv6-clion/bio.c" "C:/Users/yeeha/CLionProjects/xv6-clion/cmake-build-debug/CMakeFiles/xv6_clion.dir/bio.c.obj"
+  "C:/Users/yeeha/CLionProjects/xv6-clion/bootmain.c" "C:/Users/yeeha/CLionProjects/xv6-clion/cmake-build-debug/CMakeFiles/xv6_clion.dir/bootmain.c.obj"
+  "C:/Users/yeeha/CLionProjects/xv6-clion/cat.c" "C:/Users/yeeha/CLionProjects/xv6-clion/cmake-build-debug/CMakeFiles/xv6_clion.dir/cat.c.obj"
+  "C:/Users/yeeha/CLionProjects/xv6-clion/console.c" "C:/Users/yeeha/CLionProjects/xv6-clion/cmake-build-debug/CMakeFiles/xv6_clion.dir/console.c.obj"
+  "C:/Users/yeeha/CLionProjects/xv6-clion/echo.c" "C:/Users/yeeha/CLionProjects/xv6-clion/cmake-build-debug/CMakeFiles/xv6_clion.dir/echo.c.obj"
+  "C:/Users/yeeha/CLionProjects/xv6-clion/exec.c" "C:/Users/yeeha/CLionProjects/xv6-clion/cmake-build-debug/CMakeFiles/xv6_clion.dir/exec.c.obj"
+  "C:/Users/yeeha/CLionProjects/xv6-clion/file.c" "C:/Users/yeeha/CLionProjects/xv6-clion/cmake-build-debug/CMakeFiles/xv6_clion.dir/file.c.obj"
+  "C:/Users/yeeha/CLionProjects/xv6-clion/forktest.c" "C:/Users/yeeha/CLionProjects/xv6-clion/cmake-build-debug/CMakeFiles/xv6_clion.dir/forktest.c.obj"
+  "C:/Users/yeeha/CLionProjects/xv6-clion/fs.c" "C:/Users/yeeha/CLionProjects/xv6-clion/cmake-build-debug/CMakeFiles/xv6_clion.dir/fs.c.obj"
+  "C:/Users/yeeha/CLionProjects/xv6-clion/grep.c" "C:/Users/yeeha/CLionProjects/xv6-clion/cmake-build-debug/CMakeFiles/xv6_clion.dir/grep.c.obj"
+  "C:/Users/yeeha/CLionProjects/xv6-clion/ide.c" "C:/Users/yeeha/CLionProjects/xv6-clion/cmake-build-debug/CMakeFiles/xv6_clion.dir/ide.c.obj"
+  "C:/Users/yeeha/CLionProjects/xv6-clion/init.c" "C:/Users/yeeha/CLionProjects/xv6-clion/cmake-build-debug/CMakeFiles/xv6_clion.dir/init.c.obj"
+  "C:/Users/yeeha/CLionProjects/xv6-clion/ioapic.c" "C:/Users/yeeha/CLionProjects/xv6-clion/cmake-build-debug/CMakeFiles/xv6_clion.dir/ioapic.c.obj"
+  "C:/Users/yeeha/CLionProjects/xv6-clion/kalloc.c" "C:/Users/yeeha/CLionProjects/xv6-clion/cmake-build-debug/CMakeFiles/xv6_clion.dir/kalloc.c.obj"
+  "C:/Users/yeeha/CLionProjects/xv6-clion/kbd.c" "C:/Users/yeeha/CLionProjects/xv6-clion/cmake-build-debug/CMakeFiles/xv6_clion.dir/kbd.c.obj"
+  "C:/Users/yeeha/CLionProjects/xv6-clion/kill.c" "C:/Users/yeeha/CLionProjects/xv6-clion/cmake-build-debug/CMakeFiles/xv6_clion.dir/kill.c.obj"
+  "C:/Users/yeeha/CLionProjects/xv6-clion/lapic.c" "C:/Users/yeeha/CLionProjects/xv6-clion/cmake-build-debug/CMakeFiles/xv6_clion.dir/lapic.c.obj"
+  "C:/Users/yeeha/CLionProjects/xv6-clion/ln.c" "C:/Users/yeeha/CLionProjects/xv6-clion/cmake-build-debug/CMakeFiles/xv6_clion.dir/ln.c.obj"
+  "C:/Users/yeeha/CLionProjects/xv6-clion/log.c" "C:/Users/yeeha/CLionProjects/xv6-clion/cmake-build-debug/CMakeFiles/xv6_clion.dir/log.c.obj"
+  "C:/Users/yeeha/CLionProjects/xv6-clion/ls.c" "C:/Users/yeeha/CLionProjects/xv6-clion/cmake-build-debug/CMakeFiles/xv6_clion.dir/ls.c.obj"
+  "C:/Users/yeeha/CLionProjects/xv6-clion/main.c" "C:/Users/yeeha/CLionProjects/xv6-clion/cmake-build-debug/CMakeFiles/xv6_clion.dir/main.c.obj"
+  "C:/Users/yeeha/CLionProjects/xv6-clion/memide.c" "C:/Users/yeeha/CLionProjects/xv6-clion/cmake-build-debug/CMakeFiles/xv6_clion.dir/memide.c.obj"
+  "C:/Users/yeeha/CLionProjects/xv6-clion/mkdir.c" "C:/Users/yeeha/CLionProjects/xv6-clion/cmake-build-debug/CMakeFiles/xv6_clion.dir/mkdir.c.obj"
+  "C:/Users/yeeha/CLionProjects/xv6-clion/mkfs.c" "C:/Users/yeeha/CLionProjects/xv6-clion/cmake-build-debug/CMakeFiles/xv6_clion.dir/mkfs.c.obj"
+  "C:/Users/yeeha/CLionProjects/xv6-clion/mp.c" "C:/Users/yeeha/CLionProjects/xv6-clion/cmake-build-debug/CMakeFiles/xv6_clion.dir/mp.c.obj"
+  "C:/Users/yeeha/CLionProjects/xv6-clion/picirq.c" "C:/Users/yeeha/CLionProjects/xv6-clion/cmake-build-debug/CMakeFiles/xv6_clion.dir/picirq.c.obj"
+  "C:/Users/yeeha/CLionProjects/xv6-clion/pipe.c" "C:/Users/yeeha/CLionProjects/xv6-clion/cmake-build-debug/CMakeFiles/xv6_clion.dir/pipe.c.obj"
+  "C:/Users/yeeha/CLionProjects/xv6-clion/printf.c" "C:/Users/yeeha/CLionProjects/xv6-clion/cmake-build-debug/CMakeFiles/xv6_clion.dir/printf.c.obj"
+  "C:/Users/yeeha/CLionProjects/xv6-clion/proc.c" "C:/Users/yeeha/CLionProjects/xv6-clion/cmake-build-debug/CMakeFiles/xv6_clion.dir/proc.c.obj"
+  "C:/Users/yeeha/CLionProjects/xv6-clion/rm.c" "C:/Users/yeeha/CLionProjects/xv6-clion/cmake-build-debug/CMakeFiles/xv6_clion.dir/rm.c.obj"
+  "C:/Users/yeeha/CLionProjects/xv6-clion/sh.c" "C:/Users/yeeha/CLionProjects/xv6-clion/cmake-build-debug/CMakeFiles/xv6_clion.dir/sh.c.obj"
+  "C:/Users/yeeha/CLionProjects/xv6-clion/sleeplock.c" "C:/Users/yeeha/CLionProjects/xv6-clion/cmake-build-debug/CMakeFiles/xv6_clion.dir/sleeplock.c.obj"
+  "C:/Users/yeeha/CLionProjects/xv6-clion/spinlock.c" "C:/Users/yeeha/CLionProjects/xv6-clion/cmake-build-debug/CMakeFiles/xv6_clion.dir/spinlock.c.obj"
+  "C:/Users/yeeha/CLionProjects/xv6-clion/stressfs.c" "C:/Users/yeeha/CLionProjects/xv6-clion/cmake-build-debug/CMakeFiles/xv6_clion.dir/stressfs.c.obj"
+  "C:/Users/yeeha/CLionProjects/xv6-clion/string.c" "C:/Users/yeeha/CLionProjects/xv6-clion/cmake-build-debug/CMakeFiles/xv6_clion.dir/string.c.obj"
+  "C:/Users/yeeha/CLionProjects/xv6-clion/syscall.c" "C:/Users/yeeha/CLionProjects/xv6-clion/cmake-build-debug/CMakeFiles/xv6_clion.dir/syscall.c.obj"
+  "C:/Users/yeeha/CLionProjects/xv6-clion/sysfile.c" "C:/Users/yeeha/CLionProjects/xv6-clion/cmake-build-debug/CMakeFiles/xv6_clion.dir/sysfile.c.obj"
+  "C:/Users/yeeha/CLionProjects/xv6-clion/sysproc.c" "C:/Users/yeeha/CLionProjects/xv6-clion/cmake-build-debug/CMakeFiles/xv6_clion.dir/sysproc.c.obj"
+  "C:/Users/yeeha/CLionProjects/xv6-clion/test.c" "C:/Users/yeeha/CLionProjects/xv6-clion/cmake-build-debug/CMakeFiles/xv6_clion.dir/test.c.obj"
+  "C:/Users/yeeha/CLionProjects/xv6-clion/trap.c" "C:/Users/yeeha/CLionProjects/xv6-clion/cmake-build-debug/CMakeFiles/xv6_clion.dir/trap.c.obj"
+  "C:/Users/yeeha/CLionProjects/xv6-clion/uart.c" "C:/Users/yeeha/CLionProjects/xv6-clion/cmake-build-debug/CMakeFiles/xv6_clion.dir/uart.c.obj"
+  "C:/Users/yeeha/CLionProjects/xv6-clion/ulib.c" "C:/Users/yeeha/CLionProjects/xv6-clion/cmake-build-debug/CMakeFiles/xv6_clion.dir/ulib.c.obj"
+  "C:/Users/yeeha/CLionProjects/xv6-clion/umalloc.c" "C:/Users/yeeha/CLionProjects/xv6-clion/cmake-build-debug/CMakeFiles/xv6_clion.dir/umalloc.c.obj"
+  "C:/Users/yeeha/CLionProjects/xv6-clion/usertests.c" "C:/Users/yeeha/CLionProjects/xv6-clion/cmake-build-debug/CMakeFiles/xv6_clion.dir/usertests.c.obj"
+  "C:/Users/yeeha/CLionProjects/xv6-clion/vm.c" "C:/Users/yeeha/CLionProjects/xv6-clion/cmake-build-debug/CMakeFiles/xv6_clion.dir/vm.c.obj"
+  "C:/Users/yeeha/CLionProjects/xv6-clion/wc.c" "C:/Users/yeeha/CLionProjects/xv6-clion/cmake-build-debug/CMakeFiles/xv6_clion.dir/wc.c.obj"
+  "C:/Users/yeeha/CLionProjects/xv6-clion/zombie.c" "C:/Users/yeeha/CLionProjects/xv6-clion/cmake-build-debug/CMakeFiles/xv6_clion.dir/zombie.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../."
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
