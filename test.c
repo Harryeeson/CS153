38db741eedc3f15abb34fcf2b2d59016923facf5
Harryeeson <yeeharrison57@yahoo.com>
2020-04-20 16:34:08 -0700

    updated test.c

--- test.c
+++ test.c
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include "types.h"
 #include "stat.h"
 #include "user.h"
@@ -7,6 +6,3 @@ int main(int argc, char *argv[]) {
     printf(1, "helloo\n");
     exit(0);
 }
-=======
-
->>>>>>> 0ec4842b6a2b3c0e5e82528af7a3393ff47a5fee
