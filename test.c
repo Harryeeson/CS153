548157988f37c9f93f6c0f0f143d83d943e652a5
Lucas Song <lucas7song@gmail.com>
2020-04-23 13:53:21 -0700

    add test.c

--- test.c
+++ test.c
@@ -1,8 +1,36 @@
 #include "types.h"
 #include "stat.h"
 #include "user.h"
+#include "proc.h"
 
-int main(int argc, char *argv[]) {
-    printf(1, "partb\n");
-    exit(0);
+int main(int argc, char *argv[])
+{
+    // printf(1, "partb\n");
+    // exit(0);
+    int child_pid;
+    int status;
+
+    child_pid = fork();
+
+    if (child_pid == 0)
+    {
+        printf("Child PID is %ld\n", (long) getpid());
+        // in child; do stuff including perhaps exec
+    }
+    else if (child_pid == -1)
+    {
+        exit(-1);
+    }
+    else
+    {
+        if (waitpid(child_pid, &status, 0) == child_pid)
+        {
+            // child exited or interrupted; now you can do something with status
+                    printf("Parent PID is %ld\n", (long) getpid());
+        }
+        else
+        {
+            exit(-1);
+        }
+    }
 }
