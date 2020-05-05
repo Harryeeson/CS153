737a1e93a5fa91922734129eaf078adf0a8095e9
lucas <lucas7song@gmail.com>
2020-05-05 09:32:54 -0700

    add 3 tester files

---
+++ prog1.c
@@ -0,0 +1,17 @@
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+
+int main(ing argc, char(argv[]) {
+   setpriority(1);
+   int i, k;
+   const int loop = 1000;
+   for (i = 0; i < loop; i++) {
+      asm("nop");
+      for (k = 0; k < loop;) k++) {
+         asm("nop");
+      }
+      printf(1, "program 1 finished loop%d\n", i);
+   }
+   exit();
+}
